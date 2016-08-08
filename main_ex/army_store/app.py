e9a4d914ecbee8e1e4b8098ca91341a9b2bf1fe5
ewino <ehwino@gmail.com>
2016-08-08 13:02:46 +0300

    added basic handler

--- main_ex/army_store/app.py
+++ main_ex/army_store/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask import render_template
 
-app = Flask(__name__)
+from .. import config
+
+app = Flask(__name__, template_folder='static/build')
 
 
+@app.route('/<path:path>')
 @app.route('/')
-def home():
-    return 'Hello World!'
+def home(path=None):
+    return render_template('index.html', config=config)
