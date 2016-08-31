6fc763724af6c4cbe6ef41b4018b925b27f72948
ofir123 <ofirbrukner@gmail.com>
2016-08-31 13:19:00 +0300

    Improved product page and added similar products.

--- main_ex/unistore/static/app/layout/department.controller.js
+++ main_ex/unistore/static/app/layout/department.controller.js
@@ -39,26 +39,18 @@
 
             if (found) {
                 var departmentId = deptVm.currentDept;
-                departmentService.getProducts(departmentId)
-                    .then(function(response) {
-                        deptVm.products = response;
-                    })
-                    .catch(function() {
-                        console.error('Couldn\'t load products for department ' + departmentId);
-                    });
+                departmentService.getProducts(departmentId).then(function(response) {
+                    deptVm.products = response;
+                });
             }
         }
 
-        departmentService.getDepartments()
-            .then(function(response) {
-                deptVm.departments = response.departments;
-                if (deptVm.currentDept === null) {
-                    deptVm.currentDept = deptVm.departments[0].id;
-                }
-                refreshDepartment();
-            })
-            .catch(function() {
-                console.error('Couldn\'t load departments');
-            });
+        departmentService.getDepartments().then(function(response) {
+            deptVm.departments = response.departments;
+            if (deptVm.currentDept === null) {
+                deptVm.currentDept = deptVm.departments[0].id;
+            }
+            refreshDepartment();
+        });
     }
 })();
